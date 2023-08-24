ae109a170c1e70d84a2f76d77541752e876c018a
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-08-23 23:55:00 -0500

    exercises

--- experiments/e2.py
+++ experiments/e2.py
@@ -6,3 +6,21 @@ while True:
     todo = input(user_prompt)
     print(todo.title())
     todos.append(todo)
+
+todos = []
+
+while True:
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+                todo = input("Enter a todo: ")
+                todos.append(todo)
+            case 'show':
+                for item in todos:
+                    print(item)
+            case 'exit':
+                break
+
+    print("Bye")
