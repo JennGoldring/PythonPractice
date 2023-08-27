f46693b77e50346d13b8beae053e2395ef4aa8ae
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-08-27 08:54:19 -0500

    more exercises and practice

--- experiments/tryingthings.py
+++ experiments/tryingthings.py
@@ -1,9 +1,19 @@
-country = input("Enter a country: ")
+# country = input("Enter a country: ")
+#
+# match country:
+#     case "USA":
+#         print("Hello")
+#     case "India":
+#         print("Namaste")
+#     case "Germany":
+#         print("Hallo")
 
-match country:
-    case "USA":
-        print("Hello")
-    case "India":
-        print("Namaste")
-    case "Germany":
-        print("Hallo")
+# amount = input("how much?")
+# rate = 2
+# dollar = float(amount)
+# print(dollar * rate)
+
+
+ranking = ['John', 'Sen', 'Lisa']
+user_input = int(input("Which Ranking? "))
+print(ranking[user_input - 1])
