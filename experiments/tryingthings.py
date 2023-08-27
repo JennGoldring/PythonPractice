633547a5ad6f8bb00c39b9e64bd261f296c47546
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-08-27 09:53:20 -0500

    more exercises and practice

--- experiments/tryingthings.py
+++ experiments/tryingthings.py
@@ -14,6 +14,14 @@
 # print(dollar * rate)
 
 
+# ranking = ['John', 'Sen', 'Lisa']
+# user_input = int(input("Which Ranking? "))
+# print(ranking[user_input - 1])
+
 ranking = ['John', 'Sen', 'Lisa']
-user_input = int(input("Which Ranking? "))
-print(ranking[user_input - 1])
+
+user_input = input("Choose an athlete: ")
+
+if user_input in ranking:
+    position = ranking.index(user_input) + 1
+    print(position)
