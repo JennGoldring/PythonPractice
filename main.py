21e063f7d597e7b1f193be0a9ee26a1bf489dee8
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-09-18 07:59:09 -0500

    exercises and practice

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list
-    of to-do items.
-    """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """Write the to-do items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ")
@@ -18,15 +7,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -45,7 +34,7 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -61,7 +50,7 @@ while True:
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
